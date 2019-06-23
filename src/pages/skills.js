58c49dd4b21a663223ceec694b2a4dff20af571d
jimmyshi360 <jimmyshi360@gmail.com>
2019-06-23 18:32:04 -0400

    fixed image extensions

---
+++ src/pages/skills.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link } from "gatsby"
+import Footer from "./footer"
+
+const Skills = () => (
+
+ <div className="skills-wrapper"> 
+    <ul>
+		<li>
+        JS
+      </li>
+      <li>
+        React
+      </li>
+	  <li>
+        Redux
+      </li>
+	  <li>
+	    Jest
+	  </li>
+	  <li>
+        Saga
+      </li>
+      <li>
+        Python
+      </li>
+      <li>
+        Django
+      </li>
+    </ul>
+    <ul>
+		<li>
+        Java
+      </li>
+      <li>
+        JUnit
+      </li>
+	  <li>
+        C/C++
+      </li>
+    </ul>
+  </div>
+
+
+)
+
+export default Skills
