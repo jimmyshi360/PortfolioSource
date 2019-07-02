69eea3e69969e673aafb02eb4e0292c590f949c7
Jimmy Shi <shijm1@c02mj1zkfd58-ml.jhuapl.edu>
2019-07-02 08:52:54 -0400

    footer margin

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,20 +18,18 @@ class Footer extends React.Component{
 	render(){
 
 	return(
-      <footer >
+      <footer style={{marginTop: '100px'}}>
 		<div style={{textAlign: 'center'}}
 		>
 			<p style={{fontSize: isMobile ? '1em': '1.4em'}}
 
 			>
-				My portfolio site built from scratch using
+				Built from scratch with
 				<a href="https://reactjs.org/" target="_blank"> ReactJS</a>,
-				<a href="https://jquery.com/" target="_blank"> JQuery</a>,
 				<a href="https://www.w3.org/standards/webdesign/htmlcss" target="_blank"> HTML5</a>,
 				<a href="http://lesscss.org/" target="_blank"> Less</a>,
 				<a href="https://graphql.org/" target="_blank"> GraphQL</a>,
 				<a href="https://www.gatsbyjs.org/" target="_blank"> GatsbyJS</a>,
-				<a href="https://michalsnik.github.io/aos/" target="_blank"> AOS</a>,
 				<a href="https://www.adobe.com/products/photoshop.html" target="_blank"> Adobe Photoshop</a> and
 				<a href="https://www.adobe.com/products/aftereffects.html" target="_blank"> After Effects</a>
 
