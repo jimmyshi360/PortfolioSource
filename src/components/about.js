f6b21f0e4a771ddae79a6cc7a192602569180070
jimmyshi360 <jimmyshi360@gmail.com>
2019-07-09 20:49:00 -0400

    about additions

--- src/components/about.js
+++ src/components/about.js
@@ -14,7 +14,7 @@ import cmu from '../images/cmu.jpg';
 import stemAward from '../images/stemAward.jpg';
 import joshan from '../images/joshan.jpg';
 import banner from '../images/banner_campus.jpg';
-
+import foobar from '../images/foobar.jpg';
 class About extends React.Component {
 	 componentDidMount() {
 	  this.aos = AOS;
@@ -92,7 +92,7 @@ class About extends React.Component {
 		<h6>My first Hackathon! Best mobile app at HackMHS 2015</h6>
 		
 		<p style={{ fontSize: isMobile ? '0.7em' : '1em' }}>
-		Perhaps you're wondering where I'm from. I was born in Beijing, but lived around Europe, mostly in Vienna, Austria for the first few years of my life. My dad worked in the UN, so we traveled all over the east.  During my travels, I had my first experience with tech on an old iMac in Vienna playing Pac-Man. I was hooked... I knew what I wanted to do in my spare time: make games!
+		It's certainly been a long journey since I first started in tech. Perhaps you're wondering where I began and where I'm from. I was born in Beijing, but lived around Europe, mostly in Vienna, Austria for the first few years of my life. My dad worked in the UN, so we traveled all over the east.  During my travels, I had my first experience with tech on an old iMac in Vienna playing Pac-Man. I was hooked... I knew what I wanted to do in my spare time: make games!
 		</p>
 		<img
           alt="loading"
@@ -123,6 +123,21 @@ class About extends React.Component {
         />
 		<h6>National STEM Video Game Design Challenge</h6>
 		
+		<p style={{ fontSize: isMobile ? '0 .7em' : '1em' }}>
+		My career in tech was certainly kick-started by game development. I've spent many hours messing around in the Unity Game Engine. My true beginning in tech however was through my high school's Computer Science curriculum. I'm grateful for the Intro to Java Programming and Advanced Algorithms classes that I took. I got into competition programming, and participated in many competitions like the USA Computing Olympiad, UPenn PClassic, Bloomberg CodeCon, and a fun challenge called Google Foobar!
+		</p>
+		<img
+          alt="loading"
+          className="desc"
+          src={foobar}
+          className="about-image2"
+   
+        />
+		<h6>Getting ready for Foobar</h6>
+		
+		<p style={{ fontSize: isMobile ? '0 .7em' : '1em' }}>
+		In college, I got into webdesign working for HopHacks and the ACM. I've made the 2019 Spring and Fall Website Frontends and I help maintain the WiCS website.
+          </p>
 		 <p style={{ fontSize: isMobile ? '0.7em' : '1em' }}>
 Did I mention I like to draw? I used to take art classes, but I transitioned my knowledge into the digital realm. I treat everything like it's art: software, websites and even text documents. I believe beautiful design makes everything better!          </p>
 		  <img
