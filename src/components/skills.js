a4fd49a9e3410871cf48504b498fcddaed5a32a5
Jimmy Shi <shijm1@c02mj1zkfd58-ml.jhuapl.edu>
2019-07-11 15:33:46 -0400

    skills revamp

--- src/components/skills.js
+++ src/components/skills.js
@@ -25,22 +25,27 @@ class Skills extends React.Component {
         <ul style={{ listStyle: 'none' }}>
           <h5>Web Development</h5>
           <li>
-        React, Redux, Jest, JavaScript, HTML,
-        CSS/SCSS/Less (open-source, several websites
-        including Hophacks Spring and Fall 2019 frontends,
-        the JHU WiCS site, and Department of Defense mission planner site)
+        <span className="skill-item">React</span><span className="skill-item">Redux</span> 
+        <span className="skill-item">Jest</span> <span className="skill-item">JavaScript</span> <span className="skill-item">HTML</span>
+        <span className="skill-item">CSS</span><span className="skill-item">SCSS</span><span className="skill-item">LESS</span> 
+        <span className="skill-item">MaterialUI</span>
+        <span className="skill-item">Open Source</span>
+          
           </li>
           <h5>Backend/Middleware</h5>
           <li>
-        Python, Django, Redux-Saga, WebSockets
+          <span className="skill-item">Python</span><span className="skill-item">Django</span> 
+          <span className="skill-item">Redux-Saga</span> <span className="skill-item">WebSockets</span>
           </li>
           <h5>General languages </h5>
           <li>
-		    Java, JUnit, C# (Game programming), C/C++ (Coursework and robotics experience)
+          <span className="skill-item">Java</span> <span className="skill-item">JUnit</span> <span className="skill-item">C# </span> <span className="skill-item">C</span>
+          <span className="skill-item">C++</span> 
           </li>
           <h5>Digital Design Tools</h5>
           <li>
-        Adobe Photoshop (designed the JHU banner with), Illustrator and After Effects (familiar)
+          <span className="skill-item">Adobe Photoshop</span> 
+          <span className="skill-item">Adobe Illustrator and After Effects (familiar) </span>
           </li>
         </ul>
       </div>
