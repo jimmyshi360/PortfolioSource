e324171f73cc3ab97923591ec2def51e8ac72f43
jimmyshi360 <jimmyshi360@gmail.com>
2019-07-14 20:55:48 -0400

    new seo img

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
-import logo from '../images/gatsby-icon.png';
+import logo from '../images/seologo.png';
 
 function SEO({
   description, lang, meta, keywords, title,
