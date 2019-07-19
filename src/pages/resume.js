48ae8f510eb180c80f665771afb127927ffa8c28
Jimmy Shi <shijm1@c02mj1zkfd58-ml.jhuapl.edu>
2019-07-19 10:13:00 -0400

    resume link update

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ResumePage = () => (
   <div>
-    <iframe title="resume" src="https://drive.google.com/file/d/1XiR5nNfOiTCVnfoHoMnpY7pEx7aVQBsy/preview" width="100%" height="1080px" frameBorder="0" />
+    <iframe title="resume" src="https://drive.google.com/file/d/1sBrRR_tUEKis0GpviI84IQ7kW9Ew8pYW/preview" width="100%" height="1080px" frameBorder="0" />
   </div>
 );
 
